5f466f441e76e2fe8a7ff9755528ad9b909798d8
Orla Madden <omadden2@gmail.com>
2020-01-20 19:22:58 +0000

    Updated test files

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,24 @@
-function addition() {
-    return 42;
+// function addition() {
+//     return 42; // test passes, but this calculator is no good
+// }
+
+// function addition(numOne, numTwo) {
+//     if(typeof(numOne) == "number" && typeof(numTwo) == "number") {
+//         return numOne + numTwo;
+//     } else {
+//         alert("Error!");
+//     }
+// }
+
+Calculator = function() {
+    this.value = 0;
 }
+
+Calculator.prototype.add = function(number) {
+    if(typeof(number) == "number") {
+        this.value += number;
+    } else {
+        alert("Error!");
+    }
+}
+
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -8,9 +8,26 @@ I want to test a calulator
 */
 
 describe('Calculator', function() {
+
+    beforeEach(function() {
+    calc = new Calculator;
+});
+
     describe('Addition tests', function() {
         it('Should return 42', function() {
-            expect(addition(20, 22)).toBe(42);
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42);
+        });
+        it('Should return 26', function() {
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
+        });
+        it('Should return an error if we don\'t supply two numbers', function() {
+            spyOn(window, "alert"); //spies on the window alert function
+            calc.add("Hitchhikers", "Guide");
+            expect(window.alert).toHaveBeenCalledWith("Error!");
         });
     });
 });
