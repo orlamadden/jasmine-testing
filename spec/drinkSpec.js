4f8bd351fe8f13b5f28825f4d8a463711a420f61
Orla Madden <omadden2@gmail.com>
2020-01-21 06:50:24 +0000

    Updated tests

--- spec/drinkSpec.js
+++ spec/drinkSpec.js
@@ -1,6 +1,36 @@
-describe("Checks age", function() {
-    it("Should return Drink Toddy when called as whatCanIDrink(13)", function() {
-        var result = whatCanIDrink(13)
-        expect(result).toBe("Drink Toddy");
+describe("My whatCanIDrink function", function(){
+   
+    beforeEach(function() {
+       drink = new whatCanIDrink();
+   });
+
+    describe("Checks age", function() {
+        it("Should exist", function() {
+            expect(whatCanIDrink).toBeDefined();
+        });
+        it("Should return Drink Toddy when called as whatCanIDrink(13)", function() {
+            var result = whatCanIDrink(13)
+            expect(result).toBe("Drink Toddy");
+        });
+        it("Should return Drink Coke when called as whatCanIDrink(17)", function() {
+            var result = whatCanIDrink(17)
+            expect(result).toBe("Drink Coke");
+        });
+        it("Should return Drink Beer when called as whatCanIDrink(19)", function() {
+            var result = whatCanIDrink(19)
+            expect(result).toBe("Drink Beer");
+        });
+        it("Should return Drink Whiskey when called as whatCanIDrink(99)", function() {
+            var result = whatCanIDrink(99)
+            expect(result).toBe("Drink Whiskey");
+        });
+        it("Should be unable to return a drink when called as whatCanIDrink(150)", function() {
+            var result = whatCanIDrink(150)
+            expect(result).toBe("Sorry I can't tell what drink because that age is incorrect!");
+        });
+        it("Should contain Drink when called as whatCanIDrink(17)", function() {
+            var result = whatCanIDrink(10).indexOf("Drink")
+            expect(result).not.toEqual(-1);
+        });
     });
 });
